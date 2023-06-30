bb468b3ff108ead44a4d7f812818b5b4d9b985cd
mercelo13 <marcelo.garciar@sansano.usm.cl>
2023-06-30 19:30:55 -0400

    LLegue hasta la 3 :c

--- tests/routes/health/GETallcities.test.js
+++ tests/routes/health/GETallcities.test.js
@@ -22,27 +22,26 @@ describe('GET /api/cities', () => {
     // Verificar que el número de países en el test sea igual al número de países en el dataset
     expect(uniqueCountries.size).toBe(datasetCountriesCount);
   });
+});
+describe('GET /api/cities/by_country/:country', () => {
 
-  describe('GET /api/cities/by_country/:country', () => {
+  test('DEBE DEVOLVER LOS OBJETOS DE LA BÚSQUEDA', async () => {
+    const country = 'ATLANTIS'; // Reemplazar con el país de prueba
 
-    test('DEBE DEVOLVER LOS OBJETOS DE LA BÚSQUEDA', async () => {
-      const country = 'ATLANTIS'; // Reemplazar con el país de prueba
-    
-      const response = await request(app.callback()).get(`/api/cities/by_country/${country}`);
-      expect(response.status).toBe(200);
-    
-      const searchResults = response.body;
-    
-      console.log(`Resultados de la búsqueda para ${country}:`, searchResults);
-    
-      // Verificar si no se encontraron ciudades
-      if (searchResults.length === 0) {
-        // Devolver mensaje de error
-        expect(searchResults).toEqual({ message: 'No se encontraron ciudades para el país ingresado' });
-      } else {
-        // Verificar que la respuesta contenga un arreglo con los objetos de la búsqueda
-        expect(Array.isArray(searchResults)).toBe(true);
-      }
-    });
+    const response = await request(app.callback()).get(`/api/cities/by_country/${country}`);
+    expect(response.status).toBe(200);
+
+    const searchResults = response.body;
+
+    console.log(`Resultados de la búsqueda para ${country}:`, searchResults);
+
+    // Verificar si no se encontraron ciudades
+    if (searchResults.length === []) {
+      // Devolver mensaje de error
+      expect(searchResults).toEqual({ message: 'No se encontraron ciudades para el país ingresado' });
+    } else {
+      // Verificar que la respuesta contenga un arreglo con los objetos de la búsqueda
+      expect(Array.isArray(searchResults)).toBe(true);
+    }
   });
 });
